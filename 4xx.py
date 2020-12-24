fb487f24131177c42326b8ba497dca8aa6a060f8
kira2040k <46848151+kira2040k@users.noreply.github.com>
2020-12-24 14:47:46 +0300

    Update 4xx.py

--- 4xx.py
+++ 4xx.py
@@ -2,9 +2,11 @@ import requests
 import sys
 import threading
 import re
+from ipaddress import IPv4Network
 from functions import *
 if (2 <= len(sys.argv) <= 2):
     url = sys.argv[1]
+    
 else:
     print("Usage: python 4xx.py url")
     print("example 4xx.py https://google.com/admin/")
@@ -18,3 +20,7 @@ test4(url)
 for i in range(1,256):
     x = threading.Thread(target=attack,args=(i,url,))
     x.start()
+
+for addr in IPv4Network('172.16.0.0/12'):
+    x2 = threading.Thread(target=attack2,args=(addr,url,))
+    x2.start()
