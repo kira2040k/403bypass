393f48b278beaa949945e283ca24d648066f3a8a
kira2040k <46848151+kira2040k@users.noreply.github.com>
2020-12-25 17:58:46 +0300

    Update 4xx.py

--- 4xx.py
+++ 4xx.py
@@ -20,14 +20,3 @@ test4(url)
 for i in range(1,256):
     x = threading.Thread(target=attack,args=(i,url,))
     x.start()
-
-for addr in IPv4Network('172.16.0.0/12'):
-    x2 = threading.Thread(target=attack2,args=(addr,url,))
-    x2.start()
-for addr2 in IPv4Network('10.0.0.0/8'):
-    x3 = threading.Thread(target=attack3,args=(addr2,url,))
-    x3.start()
-for addr3 in IPv4Network('127.0.0.0/8'):
-    x4 = threading.Thread(target=attack4,args=(addr3,url,))
-    x4.start()
-
